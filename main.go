7c0e4a834e61b47230f47d264af391e348412016
Levi Leal <levifleal@Live.com>
2024-07-16 00:26:37 -0300

    feat: update handle structure to separate sub-package

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import "github.com/levifleal/MyFlix/router"
 
 func main() {
-
+	//initializer router
 	router.Init()
-
 }
--- router/routes.go
+++ router/routes.go
@@ -1,38 +1,18 @@
 package router
 
 import (
-	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/levifleal/MyFlix/handler"
 )
 
 func initRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 	{
-		v1.GET("/serie", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "Get",
-			})
-		})
-		v1.POST("/serie", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "Post",
-			})
-		})
-		v1.DELETE("/serie", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "Delete",
-			})
-		})
-		v1.PUT("/serie", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "Put",
-			})
-		})
-		v1.GET("/series", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"msg": "Gets",
-			})
-		})
+		v1.GET("/serie", handler.ShowSerieHandler)
+		v1.POST("/serie", handler.CreateSerieHandler)
+		v1.DELETE("/serie", handler.DeleteSerieHandler)
+		v1.PUT("/serie", handler.UpdateSerieHandler)
+		v1.GET("/series", handler.ListSeriesHandler)
 	}
 }

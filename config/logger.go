39376f20be4a271d5a544d131556606ea72b99e7
Levi Leal <levifleal@Live.com>
2024-07-18 13:29:18 -0300

    feat: add logger colored package

--- config/logger.go
+++ config/logger.go
@@ -2,63 +2,59 @@ package config
 
 import (
 	"io"
-	"log"
 	"os"
+	"time"
+
+	"github.com/charmbracelet/log"
 )
 
-type Logger struct {
-	debug  *log.Logger
-	info   *log.Logger
-	warn   *log.Logger
-	err    *log.Logger
-	writer io.Writer
-}
+type Logger = log.Logger
 
-func NewLogger(p string) *Logger {
+func NewLogger(p string) *log.Logger {
 	writer := io.Writer(os.Stdout)
-	logger := log.New(writer, p, log.Ldate|log.Ltime)
+	logger := log.NewWithOptions(writer, log.Options{
+		Prefix:          p,
+		Level:           log.DebugLevel,
+		TimeFormat:      time.Kitchen,
+		ReportCaller:    true,
+		ReportTimestamp: true,
+	})
 
-	return &Logger{
-		debug:  log.New(writer, "[ Debbug ] ", logger.Flags()),
-		info:   log.New(writer, "[ Info ] ", logger.Flags()),
-		warn:   log.New(writer, "[ Warning ] ", logger.Flags()),
-		err:    log.New(writer, "[ Error ] ", logger.Flags()),
-		writer: writer,
-	}
+	return logger
 }
 
-//create Non-Formated Logs
+// //create Non-Formated Logs
 
-func (l *Logger) Debug(v ...interface{}) {
-	l.debug.Println(v...)
-}
+// func (l *Logger) Debug(v ...interface{}) {
+// 	l.debug.Println(v...)
+// }
 
-func (l *Logger) Info(v ...interface{}) {
-	l.info.Println(v...)
-}
+// func (l *Logger) Info(v ...interface{}) {
+// 	l.info.Println(v...)
+// }
 
-func (l *Logger) Warn(v ...interface{}) {
-	l.warn.Println(v...)
-}
+// func (l *Logger) Warn(v ...interface{}) {
+// 	l.warn.Println(v...)
+// }
 
-func (l *Logger) Error(v ...interface{}) {
-	l.err.Println(v...)
-}
+// func (l *Logger) Error(v ...interface{}) {
+// 	l.err.Println(v...)
+// }
 
-//create Formated enable Logs
+// //create Formated enable Logs
 
-func (l *Logger) Debugf(format string, v ...interface{}) {
-	l.debug.Printf(format, v...)
-}
+// func (l *Logger) Debugf(format string, v ...interface{}) {
+// 	l.debug.Printf(format, v...)
+// }
 
-func (l *Logger) Infof(format string, v ...interface{}) {
-	l.info.Printf(format, v...)
-}
+// func (l *Logger) Infof(format string, v ...interface{}) {
+// 	l.info.Printf(format, v...)
+// }
 
-func (l *Logger) Warnf(format string, v ...interface{}) {
-	l.warn.Printf(format, v...)
-}
+// func (l *Logger) Warnf(format string, v ...interface{}) {
+// 	l.warn.Printf(format, v...)
+// }
 
-func (l *Logger) Errorf(format string, v ...interface{}) {
-	l.err.Printf(format, v...)
-}
+// func (l *Logger) Errorf(format string, v ...interface{}) {
+// 	l.err.Printf(format, v...)
+// }
